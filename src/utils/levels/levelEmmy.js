bdf98d0f74c27df21a287a051ee65c23df09287e
leightonglim <43887684+leightonglim@users.noreply.github.com>
2025-03-07 16:33:38 -0600

    Create levelEmmy.js

---
+++ src/utils/levels/levelEmmy.js
@@ -0,0 +1,166 @@
+
+import { altoMain, anakiwa, bittersweet, celadon, chardonnay, halfBaked, lightOrchid, lightWisteria, nomad, saharaSand, turquoiseBlue } from "../colors";
+
+const levelEmmy = {
+  size: 11,
+  colorRegions: [
+  [
+    "D",
+    "D",
+    "D",
+    "I",
+    "I",
+    "I",
+    "I",
+    "K",
+    "K",
+    "K",
+    "K"
+  ],
+  [
+    "D",
+    "E",
+    "A",
+    "I",
+    "I",
+    "I",
+    "A",
+    "K",
+    "K",
+    "K",
+    "K"
+  ],
+  [
+    "D",
+    "D",
+    "D",
+    "A",
+    "I",
+    "A",
+    "A",
+    "K",
+    "K",
+    "K",
+    "K"
+  ],
+  [
+    "D",
+    "E",
+    "A",
+    "J",
+    "A",
+    "J",
+    "A",
+    "K",
+    "K",
+    "K",
+    "K"
+  ],
+  [
+    "D",
+    "D",
+    "D",
+    "J",
+    "J",
+    "J",
+    "A",
+    "C",
+    "K",
+    "K",
+    "C"
+  ],
+  [
+    "F",
+    "F",
+    "A",
+    "B",
+    "J",
+    "J",
+    "A",
+    "B",
+    "C",
+    "K",
+    "C"
+  ],
+  [
+    "F",
+    "F",
+    "F",
+    "B",
+    "B",
+    "J",
+    "B",
+    "B",
+    "H",
+    "C",
+    "C"
+  ],
+  [
+    "F",
+    "F",
+    "F",
+    "B",
+    "G",
+    "B",
+    "G",
+    "B",
+    "H",
+    "H",
+    "C"
+  ],
+  [
+    "F",
+    "F",
+    "F",
+    "B",
+    "G",
+    "G",
+    "G",
+    "B",
+    "H",
+    "H",
+    "C"
+  ],
+  [
+    "F",
+    "F",
+    "F",
+    "B",
+    "G",
+    "G",
+    "G",
+    "B",
+    "H",
+    "H",
+    "C"
+  ],
+  [
+    "F",
+    "F",
+    "F",
+    "F",
+    "G",
+    "G",
+    "G",
+    "G",
+    "H",
+    "H",
+    "H"
+  ]
+],
+  regionColors: {
+    A: lightWisteria,
+    B: chardonnay,
+    C: anakiwa,
+    D: celadon,
+    E: altoMain,
+    F: bittersweet,
+    G: saharaSand,
+    H: nomad,
+    I: lightOrchid,
+    J: halfBaked,
+    K: turquoiseBlue
+  }
+};
+
+export default levelEmmy;
